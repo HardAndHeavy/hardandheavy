b074c1feefeae3beceae612551268c6085949a3f
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2025-01-07 12:49:16 +0300

    update name

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `grig blog`,
+        name: `HardAndHeavy`,
         short_name: `Григорьев`,
         start_url: `/`,
         background_color: `#ffffff`,
